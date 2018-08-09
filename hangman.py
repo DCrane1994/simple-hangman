3fd2ec786994dd6fa68771ad6d75d37e90753a15
DCrane1994 <suffolkanon@gmail.com>
2018-08-09 11:58:24 +0100

    Add win/lose states and formatting

--- hangman.py
+++ hangman.py
@@ -22,25 +22,36 @@ guessedLetters = []
 lives = 10
 
 while gameOver == False:
-	print("Enter a guess")
-	print("You have " + str(lives) + " lives remaining.")
+	print("You have " + str(lives) + " lives remaining.\n")
 	print("Current word state:\n")
-	print(guessed_word)
+	# Strip brackets and commas to print out
+	print(*guessed_word, sep='')
+	print("\nEnter a guess")
 
-	guess = input().lower()
 	# Check that the input was a letter and account for upper case
+	guess = input().lower()
+	# Check the input is only 1 character
+	if len(guess) != 1:
+		print("Please only enter one letter at a time")
+		continue
+	# If the guess is a letter and hasn't already been guessed, proceed
 	if guess.isalpha() and guess not in guessedLetters:
 		# Go through the secret word
 		for letter in range(len(secretWord)):
 			# See if the guessed letter is in the word
 			if guess == secretWord[letter]:
-				print("Correct")
+				print("Correct\n")
 				# Replace the - with the letter and add letter to history
 				guessed_word[letter] = guess
 				guessedLetters.append(guess)
+				# if all the - have been replaced, it means they guessed the word
+				if "-" not in guessed_word:
+					print("Congratulations, you win!")
+					print("You correctly guessed the word " + secretWord)
+					gameOver = True
 		# If the letter isn't in the word, subtract 1 from lives and add letter to history
 		if guess not in secretWord:
-				print("Incorrect")
+				print("Incorrect\n")
 				lives-=1
 				guessedLetters.append(guess)
 		# If lives = 0, game over!
